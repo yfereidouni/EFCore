332c935036d9497193f09c33a3864ab47c4c21f4
Yasser Fereidouni <yasser.fereidouni@me.com>
2021-07-04 00:09:50 +0430

    EntityFramework Core : Creating Databse and tables

---
+++ EFCore/DAL/PersonDB.cs
@@ -0,0 +1,22 @@
+﻿using EFCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.DAL
+{
+    public class PersonDB : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source =.; Initial Catalog = EFCore; Integrated Security = True");
+            
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
